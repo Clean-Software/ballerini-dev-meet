cee271c506fbbc73524317081fd5cab8b1596e1b
hericklima22 <herick.lima77@gmail.com>
2022-03-02 13:19:14 -0300

    🎨 improve: event page style

--- views/eventPage/index.tsx
+++ views/eventPage/index.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, TouchableHighlight } from "react-native";
+import { View, Text, TouchableHighlight, TouchableOpacity, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { arrowWhite } from "../../assets/CDN"
 import styles from "./styles";
 
 export default function EventPage() {
@@ -12,13 +13,11 @@ export default function EventPage() {
           <Text style={styles.data}>02/MAR</Text>
           <Text style={styles.hora}>19:00</Text>
         </View>
-        <TouchableHighlight onPress={() => {navigation.goBack()}}>
-          <View style={styles.botaoVoltar}>
-            <Text style={styles.textoBotao}>Voltar</Text>
-          </View>
-        </TouchableHighlight>
+        <TouchableOpacity style={styles.botaoVoltar} onPress={() => {navigation.goBack()}}>
+          <Text style={styles.textoBotao}>Voltar</Text>
+        </TouchableOpacity>
       </View>
-      <View>
+      <View style={styles.viewCentral}>
         <Text style={styles.textoCentral}>
           Criando interfaces muito malucas com o Figma!
         </Text>
@@ -26,19 +25,25 @@ export default function EventPage() {
           Você pode criar interfaces malucas que dispertam sua criativade.
           Usando de recursos do próprio figma, como seus plugins.
         </Text>
-        <Text style={styles.byText}>Organizado por:</Text>
-        <Text style={styles.name}>Comunidade Ballerini</Text>
+        <View style={styles.byView}>
+          <Text style={styles.byText}>Organizado por:</Text>
+          <Text style={styles.name}> Comunidade Ballerini</Text>
+        </View>
       </View>
       <View style={styles.evento}>
         <Text style={styles.eventText}>Link do evento</Text>
         <View style={styles.linkView}>
           <Text style={styles.linkText}>meet.google/12345</Text>
-          <TouchableHighlight onPress={() => {}}>
-            <View style={styles.notificacoes}></View>
-          </TouchableHighlight>
-          <TouchableHighlight onPress={() => {}}>
-            <View style={styles.goToLink}></View>
-          </TouchableHighlight>
+          <View style={styles.buttons}>
+            <TouchableOpacity style={styles.btnNotificacao} onPress={() => {}}>
+              <View style={styles.notificacoes}></View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.btnGo} onPress={() => {}} >
+              <View  style={styles.goToLink}>
+                <Image source={require(arrowWhite)} />
+              </View>
+            </TouchableOpacity>
+          </View>  
         </View>
       </View>
     </SafeAreaView>
--- views/eventPage/styles.ts
+++ views/eventPage/styles.ts
@@ -1,27 +1,107 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  globalView: { margin: 30 },
+  globalView: { margin: 35 },
   topbar: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
   },
   datahora: {},
-  data: { color: "#FF5100", fontSize: 24 },
-  hora: { color: "#FF5100", fontSize: 24 },
-  botaoVoltar: {},
-  textoBotao: {},
-  textoCentral: {},
-  infoCentral: {},
-  byText: {},
-  name: {},
-  evento: {},
-  eventText: {},
-  linkView: {},
-  linkText: {},
-  notificacoes: {},
-  goToLink: {},
+  data: { 
+    color: "#FF5100", 
+    fontSize: 24 
+  },
+  hora: { 
+    color: "#FF5100", 
+    fontSize: 24 
+  },
+  botaoVoltar: {
+    alignItems: "center", 
+    justifyContent: "center"
+  },
+  textoBotao: {
+    fontSize: 18
+  },
+  viewCentral: {
+    marginTop: 40
+  },
+  textoCentral: {
+    fontSize: 32, 
+    textAlign: "auto", 
+    fontWeight: "bold"
+  },
+  infoCentral: {
+    marginTop: 20, 
+    color: "#959595", 
+    lineHeight: 27, 
+    fontSize: 14
+  },
+  byView: {
+    display: "flex", 
+    flexDirection: "row", 
+    marginTop: 20
+  },
+  byText: {
+    fontSize: 13
+  },
+  name: {
+    fontSize: 13, 
+    fontWeight: "bold"
+  },
+  evento: {
+    display: "flex", 
+    alignItems: "center", 
+    marginTop: 45
+  },
+  eventText: {
+    fontSize: 28, 
+    fontWeight: "bold"
+  },
+  linkView: {
+    backgroundColor: "#323232", 
+    borderRadius: 4, 
+    display: "flex", 
+    flexDirection: "row", 
+    justifyContent: "space-between",
+    alignSelf: "stretch", 
+    marginTop: 20, 
+    height: 50
+  },
+  
+  linkText: {
+    color: 'rgba(255, 255, 255, 0.7)', 
+    fontSize: 16, 
+    marginLeft: 14, 
+    alignSelf: "center"
+  },
+  buttons: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: 70,
+    margin: 20,
+  },
+  notificacoes: {
+    
+  },
+  btnNotificacao: {
+    borderRadius: 6,
+    width: 28, 
+    height: 28,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    alignSelf: "center"
+  },
+  goToLink: {
+
+  },
+  btnGo: {
+    borderRadius: 6,
+    width: 28, 
+    height: 28,
+    backgroundColor: 'rgba(4, 211, 97, 0.2)',
+    alignSelf: "center"
+  },
 });
 
 export default styles;
