393c4f145b3195b266273023ef0b8e11157af73b
Jonatas A <jonatasaraujofx@gmail.com>
2022-03-02 11:32:27 -0300

    ✨ add: avaliableEvents to views

--- views/index.ts
+++ views/index.ts
@@ -2,3 +2,4 @@ export { default as LoadingPage } from "./loadingPage";
 export { default as Home } from "./home";
 export { default as EventPage } from "./eventPage";
 export { default as ChooseEvent } from "./chooseEvent";
+export { default as AvaliableEvents } from "./avaliableEvents";
