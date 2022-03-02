8eb420266c995f79fbd82658f782c5fb3994f6fe
Carlos Zoft <carlosk2gunn@hotmail.com>
2022-03-02 11:36:58 -0300

    ✨ feat: add fonts at project

--- App.tsx
+++ App.tsx
@@ -1,31 +1,12 @@
-import { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
+import { useCustomFonts } from "./helpers";
 import Routes from "./navigation";
 
-const fecthFonts = () => {
-  return Font.loadAsync({
-    "Ubuntu-Regular": require("./assets/fonts/Ubuntu-Regular.ttf"),
-    "Ubuntu-Bold": require("./assets/fonts/Ubuntu-Bold.ttf"),
-    "Ubuntu-Light": require("./assets/fonts/Ubuntu-Light.ttf"),
-    "Ubuntu-Medium": require("./assets/fonts/Ubuntu-Medium.ttf"),
-    "Ubuntu-Italic": require("./assets/fonts/Ubuntu-Italic.ttf"),
-  });
-};
-
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
+  const fontLoaded = useCustomFonts();
 
   if (!fontLoaded) {
-    return (
-      <AppLoading
-        startAsync={fecthFonts}
-        onFinish={() => setFontLoaded(true)}
-        onError={console.warn}
-      />
-    );
+    return <AppLoading />;
   }
 
   return <Routes />;
