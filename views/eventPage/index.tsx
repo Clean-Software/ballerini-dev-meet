ffa29e416f893d19214953a658b52fb4a120f80f
hericklima22 <herick.lima77@gmail.com>
2022-03-02 15:16:34 -0300

    🎨 event screen finish

--- views/eventPage/index.tsx
+++ views/eventPage/index.tsx
@@ -1,49 +1,105 @@
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, TouchableHighlight, TouchableOpacity, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { arrowWhite } from "../../assets/CDN"
+import ButtonGo from "../../components/button"
+import {arrowWhiteSvg, arrowOrangeSvg} from "../../assets/CDN/svg"
+
 import styles from "./styles";
 
 export default function EventPage() {
   const navigation = useNavigation()
   return (
-    <SafeAreaView style={styles.globalView}>
-      <View style={styles.topbar}>
-        <View style={styles.datahora}>
-          <Text style={styles.data}>02/MAR</Text>
-          <Text style={styles.hora}>19:00</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.globalView}>
+        <View style={styles.topbar}>
+          <View style={styles.datahora}>
+            <Text style={styles.data}>02/MAR</Text>
+            <Text style={styles.hora}>19:00</Text>
+          </View>
+          {/* <TouchableOpacity style={styles.botaoVoltar} onPress={() => {navigation.goBack()}}>
+            <Text style={styles.textoBotao}>Voltar</Text>
+          </TouchableOpacity> */}
+          <TouchableHighlight  underlayColor='transparent' onPress={() => {
+          navigation.goBack();
+        }}>
+          <View style={styles.botaoVoltar}>
+            <Image style={styles.iconBotao} source={{
+              uri: 'https://res.cloudinary.com/dcy5jian9/image/upload/v1646193820/Arrow-White_vapvlb.png'
+            }} />
+            <Text style={styles.textoBotao}>
+              Voltar
+            </Text>
+          </View>
+        </TouchableHighlight>
         </View>
-        <TouchableOpacity style={styles.botaoVoltar} onPress={() => {navigation.goBack()}}>
-          <Text style={styles.textoBotao}>Voltar</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.viewCentral}>
-        <Text style={styles.textoCentral}>
-          Criando interfaces muito malucas com o Figma!
-        </Text>
-        <Text style={styles.infoCentral}>
-          Você pode criar interfaces malucas que dispertam sua criativade.
-          Usando de recursos do próprio figma, como seus plugins.
-        </Text>
-        <View style={styles.byView}>
-          <Text style={styles.byText}>Organizado por:</Text>
-          <Text style={styles.name}> Comunidade Ballerini</Text>
+        <View style={styles.viewCentral}>
+          <Text style={styles.textoCentral}>
+            Criando interfaces muito malucas com o Figma!
+          </Text>
+          <Text style={styles.infoCentral}>
+            Você pode criar interfaces malucas que dispertam sua criativade.
+            Usando de recursos do próprio figma, como seus plugins.
+          </Text>
+          <View style={styles.byView}>
+            <Text style={styles.byText}>Organizado por:</Text>
+            <Text style={styles.name}> Comunidade Ballerini</Text>
+          </View>
         </View>
-      </View>
-      <View style={styles.evento}>
-        <Text style={styles.eventText}>Link do evento</Text>
-        <View style={styles.linkView}>
-          <Text style={styles.linkText}>meet.google/12345</Text>
-          <View style={styles.buttons}>
-            <TouchableOpacity style={styles.btnNotificacao} onPress={() => {}}>
-              <View style={styles.notificacoes}></View>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.btnGo} onPress={() => {}} >
-              <View  style={styles.goToLink}>
-                <Image source={require(arrowWhite)} />
+        <View style={styles.evento}>
+          <Text style={styles.eventText}>Link do evento</Text>
+          <View style={styles.linkView}>
+            <Text style={styles.linkText}>meet.google/12345</Text>
+            <View style={styles.buttons}>
+              <ButtonGo image={arrowOrangeSvg} size={19} backgroundColor={'rgba(255, 255, 255, 0.2)'} style={{borderRadius: 8}} />
+              <ButtonGo image={arrowWhiteSvg} size={19} backgroundColor={'rgba(4, 211, 97, 0.2)'} style={{borderRadius: 8}} />
+            </View>  
+          </View>
+        </View>
+        <View style={styles.progressContainerView}>
+          <View style={styles.progressText}>
+            <Text style={{fontWeight: "bold", color: "#fff", fontSize: 25}}>Tempo até o evento</Text>
+            <View style={styles.time}>
+
+              <View style={{display: "flex", flexDirection: "row"}}>
+                <Text style={styles.number}>
+                  02
+                </Text>
+                <View style={{alignSelf: "center"}}>
+                  <Text style={styles.label}>
+                    DAY(s)
+                  </Text>
+                </View>
               </View>
-            </TouchableOpacity>
-          </View>  
+
+              <View style={{display: "flex", flexDirection: "row"}}>
+                <Text style={styles.number}>
+                  12
+                </Text>
+                <View style={{alignSelf: "center"}}> 
+                  <Text style={styles.label}>
+                    HOURS(s)
+                  </Text>
+                </View>
+              </View>
+
+              <View style={{display: "flex", flexDirection: "row"}}>
+                <Text style={styles.number}>
+                  30
+                </Text>
+                <View style={{alignSelf: "center"}}>
+                  <Text style={styles.label}>
+                    MIN(s)
+                  </Text>
+                </View>
+              </View>
+              
+            </View>
+          </View>
+          <View style={styles.parentBarView}>
+            <View style={styles.sonBarView}>
+
+            </View>
+          </View>
         </View>
       </View>
     </SafeAreaView>
--- views/eventPage/styles.ts
+++ views/eventPage/styles.ts
@@ -1,7 +1,14 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  globalView: { margin: 35 },
+  container: {
+    backgroundColor: "#282828",
+    display: "flex",
+    flex: 1
+  },
+  globalView: { 
+    margin: 35, 
+    },
   topbar: {
     display: "flex",
     flexDirection: "row",
@@ -16,12 +23,9 @@ const styles = StyleSheet.create({
     color: "#FF5100", 
     fontSize: 24 
   },
-  botaoVoltar: {
-    alignItems: "center", 
-    justifyContent: "center"
-  },
   textoBotao: {
-    fontSize: 18
+    fontSize: 18,
+    color: "#fff"
   },
   viewCentral: {
     marginTop: 40
@@ -29,7 +33,8 @@ const styles = StyleSheet.create({
   textoCentral: {
     fontSize: 32, 
     textAlign: "auto", 
-    fontWeight: "bold"
+    fontWeight: "bold",
+    color: "#fff"
   },
   infoCentral: {
     marginTop: 20, 
@@ -43,20 +48,23 @@ const styles = StyleSheet.create({
     marginTop: 20
   },
   byText: {
-    fontSize: 13
+    fontSize: 13,
+    color: "#fff"
   },
   name: {
     fontSize: 13, 
-    fontWeight: "bold"
+    fontWeight: "bold",
+    color: "#fff"
   },
   evento: {
     display: "flex", 
     alignItems: "center", 
-    marginTop: 45
+    marginTop: 45,
   },
   eventText: {
     fontSize: 28, 
-    fontWeight: "bold"
+    fontWeight: "bold",
+    color: "#fff"
   },
   linkView: {
     backgroundColor: "#323232", 
@@ -81,6 +89,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     width: 70,
     margin: 20,
+    alignItems: "center"
   },
   notificacoes: {
     
@@ -92,8 +101,13 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(255, 255, 255, 0.2)',
     alignSelf: "center"
   },
+  linkSvg: {
+    width: 25,
+    height: 25,
+    margin: 0
+  },
   goToLink: {
-
+    margin: 0
   },
   btnGo: {
     borderRadius: 6,
@@ -102,6 +116,62 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(4, 211, 97, 0.2)',
     alignSelf: "center"
   },
+  progressContainerView: {
+    marginTop: 100
+  },
+  progressText: {
+
+  },
+  parentBarView: {
+    height: 12,
+    backgroundColor: "#3C3C3C",
+    borderRadius: 5,
+    marginTop: 14,
+    position: "relative"
+  },
+  iconBotao: {
+    width: 25,
+    height: 25,
+    transform: [
+      {
+        rotate: "180deg"
+      }
+    ],
+  },
+  botaoVoltar: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  sonBarView: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    bottom: 0,
+    width: "50%",
+    backgroundColor: "#FF5100",
+    borderRadius: 5 
+  },
+  time: {
+    display: "flex",
+    flexDirection: "row",
+    color: "#fff",
+    justifyContent: "space-between",
+    marginTop: 17
+  },
+  number: {
+    fontWeight: "bold",
+    color: "#fff",
+    fontSize: 59,
+
+  },
+  label: {
+    color: "#fff",
+    fontSize: 16,
+    opacity: 0.5,
+    marginLeft: 4
+  }
 });
 
 export default styles;
