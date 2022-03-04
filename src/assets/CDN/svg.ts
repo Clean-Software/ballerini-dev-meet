5bcb993244f64074220e631c166235d21116bcd9
Luiz <doginuwu@gmail.com>
2022-03-04 10:56:17 -0300

    ✨ feat: add white svg cdn icons

--- src/assets/CDN/svg.ts
+++ src/assets/CDN/svg.ts
@@ -4,14 +4,20 @@ const arrowOrangeSvg =
   "https://res.cloudinary.com/dcy5jian9/image/upload/v1646235179/Arrow-Orange_sitqpn.svg";
 const programingTerminalSvg =
   "https://res.cloudinary.com/dcy5jian9/image/upload/v1646235184/Programing-Terminal_eytijc.svg";
+const programingTerminalWhiteSvg =
+  "https://res.cloudinary.com/usufit-beta/image/upload/v1646401777/ballerini-dev-meet/Programing-Terminal-White_btezsx.svg";
 const laptopProgrammingCodeSvg =
   "https://res.cloudinary.com/dcy5jian9/image/upload/v1646235181/laptop-programming-code_wjhmth.svg";
+const laptopProgrammingCodeWhiteSvg =
+  "https://res.cloudinary.com/usufit-beta/image/upload/v1646401777/ballerini-dev-meet/laptop-programming-code-white_hlgvze.svg";
 const iPhoneXWhiteSvg =
   "https://res.cloudinary.com/dcy5jian9/image/upload/v1646235181/iPhone-X-White_rj7tmm.svg";
 const iPhoneXOrangeSvg =
   "https://res.cloudinary.com/dcy5jian9/image/upload/v1646235180/iPhone-X-Orange_i4evge.svg";
 const hierarchySvg =
   "https://res.cloudinary.com/dcy5jian9/image/upload/v1646235179/hierarchy_m8etvq.svg";
+const hierarchyWhiteSvg =
+  "https://res.cloudinary.com/usufit-beta/image/upload/v1646401777/ballerini-dev-meet/hierarchy-white_qvsyor.svg";
 const personSvg =
   "https://res.cloudinary.com/dcy5jian9/image/upload/v1646235183/person_rfplpn.svg";
 const LOGOSvg =
@@ -23,10 +29,13 @@ export {
   arrowOrangeSvg,
   arrowWhiteSvg,
   programingTerminalSvg,
+  programingTerminalWhiteSvg,
   laptopProgrammingCodeSvg,
+  laptopProgrammingCodeWhiteSvg,
   iPhoneXWhiteSvg,
   iPhoneXOrangeSvg,
   hierarchySvg,
+  hierarchyWhiteSvg,
   personSvg,
   LOGOSvg,
   PatternSvg,
--- src/components/topicCard/helper.ts
+++ src/components/topicCard/helper.ts
@@ -1,14 +1,31 @@
-import { hierarchySvg, iPhoneXOrangeSvg, iPhoneXWhiteSvg, laptopProgrammingCodeSvg, programingTerminalSvg } from "../../assets/CDN";
+import { hierarchySvg, hierarchyWhiteSvg, iPhoneXOrangeSvg, iPhoneXWhiteSvg, laptopProgrammingCodeSvg, laptopProgrammingCodeWhiteSvg, programingTerminalSvg, programingTerminalWhiteSvg } from "../../assets/CDN";
 
-const imageDicionary = (selected?: boolean) =>
-  ({
+interface ImageDictionaryProps { 
+  [key: string]: string;
+}
+
+const imageDicionary = (selected?: boolean): ImageDictionaryProps => {
+  if (selected) {
+    return {
+      PROGRAMING: programingTerminalWhiteSvg,
+      LAPTOP: laptopProgrammingCodeWhiteSvg,
+      INTEGRATION: hierarchyWhiteSvg,
+      BLOCKS_CODE_CHECKMARK: laptopProgrammingCodeWhiteSvg,
+      TERMINAL: programingTerminalWhiteSvg,
+      BEZIER_CURVE: laptopProgrammingCodeWhiteSvg,
+      MOBILE: iPhoneXWhiteSvg,
+    };
+  }
+
+  return {
     PROGRAMING: programingTerminalSvg,
     LAPTOP: laptopProgrammingCodeSvg,
     INTEGRATION: hierarchySvg,
     BLOCKS_CODE_CHECKMARK: laptopProgrammingCodeSvg,
     TERMINAL: programingTerminalSvg,
     BEZIER_CURVE: laptopProgrammingCodeSvg,
-    MOBILE: selected ? iPhoneXWhiteSvg : iPhoneXOrangeSvg,
-  } as any);
+    MOBILE: iPhoneXOrangeSvg,
+  };
+};
 
 export { imageDicionary };
