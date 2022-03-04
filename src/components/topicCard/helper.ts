705b52f8445a2d9dc74b234ce8d4968f2f28af7d
Luiz <doginuwu@gmail.com>
2022-03-03 22:55:42 -0300

    🐛 fix: change "integration" icon

--- src/components/topicCard/helper.ts
+++ src/components/topicCard/helper.ts
@@ -1,13 +1,14 @@
-import { iPhoneXOrangeSvg, iPhoneXWhiteSvg, laptopProgrammingCodeSvg, programingTerminalSvg } from "../../assets/CDN";
+import { hierarchySvg, iPhoneXOrangeSvg, iPhoneXWhiteSvg, laptopProgrammingCodeSvg, programingTerminalSvg } from "../../assets/CDN";
 
-const imageDicionary = (selected?: boolean) => ({
-  PROGRAMING: programingTerminalSvg,
-  LAPTOP: laptopProgrammingCodeSvg,
-  INTEGRATION: laptopProgrammingCodeSvg,
-  BLOCKS_CODE_CHECKMARK: laptopProgrammingCodeSvg,
-  TERMINAL: programingTerminalSvg,
-  BEZIER_CURVE: laptopProgrammingCodeSvg,
-  MOBILE: selected ? iPhoneXWhiteSvg : iPhoneXOrangeSvg,
-} as any);
+const imageDicionary = (selected?: boolean) =>
+  ({
+    PROGRAMING: programingTerminalSvg,
+    LAPTOP: laptopProgrammingCodeSvg,
+    INTEGRATION: hierarchySvg,
+    BLOCKS_CODE_CHECKMARK: laptopProgrammingCodeSvg,
+    TERMINAL: programingTerminalSvg,
+    BEZIER_CURVE: laptopProgrammingCodeSvg,
+    MOBILE: selected ? iPhoneXWhiteSvg : iPhoneXOrangeSvg,
+  } as any);
 
 export { imageDicionary };
